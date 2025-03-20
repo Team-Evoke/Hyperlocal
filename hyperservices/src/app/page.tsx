83592e1313614bb2f1679f8cb6e9ffe0051b2345
Sagar Navnath Gajbhar <gajbharsagar2005@gmail.com>
2025-03-20 10:46:17 +0530

    adder the about and home

--- hyperservices/src/app/page.tsx
+++ hyperservices/src/app/page.tsx
@@ -1,7 +1,26 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      {/* Navbar */}
+      <nav style={{ display: "flex", justifyContent: "space-between", padding: "10px 20px", background: "#333", color: "white" }}>
+        <h2>Hyper Delivery</h2>
+        <div>
+          <Link href="/" style={{ color: "white", marginRight: "15px", textDecoration: "none" }}>Home</Link>
+          <Link href="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
+        </div>
+      </nav>
+      
+      {/* Main Content */}
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <h1>Hello World</h1>
+        <Link href="/about">
+          <button style={{ padding: "10px", marginTop: "10px", cursor: "pointer" }}>
+            Go to About Page
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 }
